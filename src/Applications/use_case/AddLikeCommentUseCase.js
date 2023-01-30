bb389a31010976d687e78b85631a2673e212764b
luthfirohmatulloh19 <luthfirohmatulloh19@gmail.com>
2023-01-30 11:05:16 +0700

    AddLikeCommentUseCase: add dependency injection to constructor

--- src/Applications/use_case/AddLikeCommentUseCase.js
+++ src/Applications/use_case/AddLikeCommentUseCase.js
@@ -1,7 +1,11 @@
 const AddLikeComment = require('../../Domains/comments/entities/AddLikeComment');
 
 class AddLikeCommentUseCase {
-  constructor() {}
+  constructor({ threadRepository, commentRepository, likeRepository }) {
+    this._threadRepository = threadRepository;
+    this._commentRepository = commentRepository;
+    this._likeRepository = likeRepository;
+  }
 
   async execute(useCasePayload) {}
 }
